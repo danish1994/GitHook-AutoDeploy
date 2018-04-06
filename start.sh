0d3148ee09103333ee8cae85d5a688d1d1523284
Danish <danish8802204230@gmail.com>
2018-04-06 13:38:30 +0530

    start scrpt

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 npm i
 
-ps aux | grep "node index.js $1" | awk '{print $2}' | sudo xargs kill
+# ps aux | grep "node index.js $1" | awk '{print $2}' | sudo xargs kill
 
 nohup node index.js $1 $2 >> ~/log.txt 2>&1 &
